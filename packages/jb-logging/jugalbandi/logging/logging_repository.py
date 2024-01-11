990f6118e9119fa655bed43913b9b0480d3c1bdf
Karanraj <karanraj.m@thoughtworks.com>
2024-01-11 13:06:04 +0530

    [Karan|Arun] Modify logging schema

--- packages/jb-logging/jugalbandi/logging/logging_repository.py
+++ packages/jb-logging/jugalbandi/logging/logging_repository.py
@@ -46,44 +46,77 @@ class LoggingRepository():
                     phone_number BIGINT UNIQUE,
                     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
                 );
-                CREATE TABLE IF NOT EXISTS jb_document_store_logs(
+                CREATE TABLE IF NOT EXISTS jb_document_store_log(
                     user_id INTEGER,
                     app_id INTEGER,
                     uuid TEXT PRIMARY KEY,
                     documents_list TEXT[],
                     total_file_size FLOAT,
-                    error_message TEXT,
+                    status_code INTEGER,
+                    status_message TEXT,
                     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
                     FOREIGN key (user_id) REFERENCES jb_users(id),
                     FOREIGN key (app_id) REFERENCES jb_app(id)
                 );
-                CREATE TABLE IF NOT EXISTS jb_qa_logs(
+                CREATE TABLE IF NOT EXISTS jb_qa_log(
                     id SERIAL PRIMARY KEY,
                     user_id INTEGER,
                     app_id INTEGER,
                     document_uuid TEXT,
                     input_language TEXT DEFAULT 'en',
-                    output_format TEXT DEFAULT 'TEXT',
-                    audio_input_link TEXT,
-                    stt_model_name TEXT,
                     query TEXT,
-                    query_translation_model_name TEXT,
-                    query_in_english TEXT,
+                    audio_input_link TEXT,
+                    response TEXT,
+                    audio_output_link TEXT,
                     retrieval_k_value INTEGER,
                     retrieved_chunks TEXT[],
                     prompt TEXT,
                     gpt_model_name TEXT,
-                    response_in_english TEXT,
-                    response_translation_model_name TEXT,
-                    response TEXT,
-                    tts_model_name TEXT,
-                    audio_output_link TEXT,
-                    error_message TEXT,
                     status_code INTEGER,
+                    status_message TEXT,
+                    response_time INTEGER,
                     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                    FOREIGN key (user_id) REFERENCES jb_users(id),
+                    FOREIGN KEY (user_id) REFERENCES jb_users(id),
                     FOREIGN KEY (app_id) REFERENCES jb_app(id),
-                    FOREIGN KEY (document_uuid) REFERENCES jb_document_store_logs(uuid)
+                    FOREIGN KEY (document_uuid) REFERENCES jb_document_store_log(uuid)
+                );
+                CREATE TABLE IF NOT EXISTS jb_stt_log(
+                    id SERIAL PRIMARY KEY,
+                    qa_log_id INTEGER,
+                    audio_input_link TEXT,
+                    model_name TEXT,
+                    text TEXT,
+                    status_code INTEGER,
+                    status_message TEXT,
+                    response_time INTEGER,
+                    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                    FOREIGN KEY (qa_log_id) REFERENCES jb_qa_log(id)
+                );
+                CREATE TABLE IF NOT EXISTS jb_tts_log(
+                    id SERIAL PRIMARY KEY,
+                    qa_log_id INTEGER,
+                    text TEXT,
+                    model_name TEXT,
+                    audio_output_link TEXT,
+                    status_code INTEGER,
+                    status_message TEXT,
+                    response_time INTEGER,
+                    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                    FOREIGN KEY (qa_log_id) REFERENCES jb_qa_log(id)
+                );
+                CREATE TABLE IF NOT EXISTS jb_translator_log(
+                    id SERIAL PRIMARY KEY,
+                    qa_log_id INTEGER,
+                    text TEXT,
+                    input_language TEXT,
+                    output_language TEXT,
+                    model_name TEXT,
+                    translated_text TEXT,
+                    status_code INTEGER,
+                    status_message TEXT,
+                    response_time INTEGER,
+                    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                    FOREIGN KEY (qa_log_id) REFERENCES jb_qa_log(id)
                 );
                 CREATE TABLE IF NOT EXISTS jb_chat_history(
                     id SERIAL PRIMARY KEY,
@@ -98,16 +131,19 @@ class LoggingRepository():
                     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
                     FOREIGN key (user_id) REFERENCES jb_users(id),
                     FOREIGN KEY (app_id) REFERENCES jb_app(id),
-                    FOREIGN KEY (document_uuid) REFERENCES jb_document_store_logs(uuid)
+                    FOREIGN KEY (document_uuid) REFERENCES jb_document_store_log(uuid)
                 );
 
                 CREATE INDEX IF NOT EXISTS jb_users_phone_number_idx ON jb_users(phone_number);
                 CREATE INDEX IF NOT EXISTS jb_users_first_name_idx ON jb_users(first_name);
                 CREATE INDEX IF NOT EXISTS jb_app_phone_number_idx ON jb_app(phone_number);
-                CREATE INDEX IF NOT EXISTS jb_document_store_logs_uuid_idx ON jb_document_store_logs(uuid);
-                CREATE INDEX IF NOT EXISTS jb_qa_logs_user_id_idx ON jb_qa_logs(user_id);
-                CREATE INDEX IF NOT EXISTS jb_qa_logs_app_id_idx ON jb_qa_logs(app_id);
-                CREATE INDEX IF NOT EXISTS jb_qa_logs_document_uuid_idx ON jb_qa_logs(document_uuid);
+                CREATE INDEX IF NOT EXISTS jb_document_store_log_uuid_idx ON jb_document_store_log(uuid);
+                CREATE INDEX IF NOT EXISTS jb_qa_log_user_id_idx ON jb_qa_log(user_id);
+                CREATE INDEX IF NOT EXISTS jb_qa_log_app_id_idx ON jb_qa_log(app_id);
+                CREATE INDEX IF NOT EXISTS jb_qa_log_document_uuid_idx ON jb_qa_log(document_uuid);
+                CREATE INDEX IF NOT EXISTS jb_stt_log_qa_log_id_idx ON jb_stt_log(qa_log_id);
+                CREATE INDEX IF NOT EXISTS jb_tts_log_qa_log_id_idx ON jb_tts_log(qa_log_id);
+                CREATE INDEX IF NOT EXISTS jb_translator_log_qa_log_id_idx ON jb_translator_log(qa_log_id);
                 CREATE INDEX IF NOT EXISTS jb_chat_history_user_id_idx ON jb_chat_history(user_id);
                 CREATE INDEX IF NOT EXISTS jb_chat_history_app_id_idx ON jb_chat_history(app_id);
                 CREATE INDEX IF NOT EXISTS jb_chat_history_document_uuid_idx ON jb_chat_history(document_uuid);
@@ -146,68 +182,130 @@ class LoggingRepository():
                 datetime.now(pytz.UTC),
             )
 
-    async def insert_document_store_logs(self, user_id: int, app_id: int, uuid: str,
-                                         documents_list: list, total_file_size: int,
-                                         error_message: str):
+    async def insert_document_store_log(self, user_id: int, app_id: int, uuid: str,
+                                        documents_list: list, total_file_size: int,
+                                        status_code: int, status_message: str):
         engine = await self._get_engine()
         async with engine.acquire() as connection:
             await connection.execute(
                 """
-                INSERT INTO jb_document_store_logs
+                INSERT INTO jb_document_store_log
                 (user_id, app_id, uuid, documents_list,
-                total_file_size, error_message, created_at)
-                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                total_file_size, status_code, status_message, created_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
                 """,
                 user_id,
                 app_id,
                 uuid,
                 documents_list,
                 total_file_size,
-                error_message,
+                status_code,
+                status_message,
                 datetime.now(pytz.UTC),
             )
 
-    async def insert_qa_logs(self, user_id: int, app_id: int, document_uuid: str, input_language: str,
-                             output_format: str, audio_input_link: str, stt_model_name: str,
-                             query: str, query_translation_model_name: str, query_in_english: str,
-                             retrieval_k_value: int, retrieved_chunks: list, prompt: str, gpt_model_name: str,
-                             response_in_english: str, response_translation_model_name: str, response: str,
-                             tts_model_name: str, audio_output_link: str, error_message: str, status_code: int):
+    async def insert_qa_log(self, user_id: int, app_id: int, document_uuid: str, input_language: str,
+                            query: str, audio_input_link: str, response: str, audio_output_link: str,
+                            retrieval_k_value: int, retrieved_chunks: list, prompt: str, gpt_model_name: str,
+                            status_code: int, status_message: str, response_time: int):
         engine = await self._get_engine()
         async with engine.acquire() as connection:
             await connection.execute(
                 """
-                INSERT INTO jb_qa_logs
+                INSERT INTO jb_qa_log
                 (user_id, app_id, document_uuid, input_language,
-                output_format, audio_input_link, stt_model_name,
-                query, query_translation_model_name, query_in_english,
+                query, audio_input_link, response, audio_output_link,
                 retrieval_k_value, retrieved_chunks, prompt, gpt_model_name,
-                response_in_english, response_translation_model_name, response,
-                tts_model_name, audio_output_link, error_message, status_code created_at)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
-                $15, $16, $17, $18, $19, $20, $21, $22)
+                status_code, status_message, response_time, created_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,
+                $12, $13, $14, $15, $16)
                 """,
                 user_id,
                 app_id,
                 document_uuid,
                 input_language,
-                output_format,
-                audio_input_link,
-                stt_model_name,
                 query,
-                query_translation_model_name,
-                query_in_english,
+                audio_input_link,
+                response,
+                audio_output_link,
                 retrieval_k_value,
                 retrieved_chunks,
                 prompt,
                 gpt_model_name,
-                response_in_english,
-                response_translation_model_name,
-                response,
-                tts_model_name,
-                audio_output_link,
-                error_message,
                 status_code,
+                status_message,
+                response_time,
+                datetime.now(pytz.UTC),
+            )
+
+    async def insert_stt_log(self, qa_log_id: int, audio_input_link: str,
+                             model_name: str, text: str, status_code: int,
+                             status_message: str, response_time: int):
+        engine = await self._get_engine()
+        async with engine.acquire() as connection:
+            await connection.execute(
+                """
+                INSERT INTO jb_stt_log
+                (qa_log_id, audio_input_link, model_name,
+                text, status_code, status_message,
+                response_time, created_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                """,
+                qa_log_id,
+                audio_input_link,
+                model_name,
+                text,
+                status_code,
+                status_message,
+                response_time,
+                datetime.now(pytz.UTC),
+            )
+
+    async def insert_tts_log(self, qa_log_id: int, text: str,
+                             model_name: str, audio_output_link: str, status_code: int,
+                             status_message: str, response_time: int):
+        engine = await self._get_engine()
+        async with engine.acquire() as connection:
+            await connection.execute(
+                """
+                INSERT INTO jb_tts_log
+                (qa_log_id, text, model_name,
+                audio_output_link, status_code, status_message,
+                response_time, created_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                """,
+                qa_log_id,
+                text,
+                model_name,
+                audio_output_link,
+                status_code,
+                status_message,
+                response_time,
+                datetime.now(pytz.UTC),
+            )
+
+    async def insert_translator_log(self, qa_log_id: int, text: str, input_language: str,
+                                    output_language: str, model_name: str, translated_text: str,
+                                    status_code: int, status_message: str, response_time: int):
+        engine = await self._get_engine()
+        async with engine.acquire() as connection:
+            await connection.execute(
+                """
+                INSERT INTO jb_translator_log
+                (qa_log_id, text, input_language, output_language,
+                model_name, translated_text, status_code,
+                status_message, response_time, created_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+                """,
+                qa_log_id,
+                text,
+                input_language,
+                output_language,
+                model_name,
+                translated_text,
+                status_code,
+                status_message,
+                response_time,
                 datetime.now(pytz.UTC),
             )
 
